b4787053c919918f3dc6e500805ba22957bdbc24
Yehan20 <yehansk15@gmail.com>
2023-04-21 00:14:39 +0530

    .

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import itemRouter from './router/items.router.js'
-
+import cors from 'cors'
 const app = express();
+
+app.use(cors())
 
 app.use('/',express.static("public"))
 
